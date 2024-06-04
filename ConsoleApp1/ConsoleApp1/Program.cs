91277778d1f7fa860ce515cd87c9717696ad40e1
chhimsokrith <90494699+chhimsokrith@users.noreply.github.com>
2024-06-04 09:28:46 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testCases = {
+                "[BE][FE][Urgent] there is error",
+                "[BE][QA][HAHA][Urgent] there is error"
+            };
+            foreach (string testCase in testCases)
+            {
+                Console.WriteLine($"Input: \"{testCase}\"");
+                Console.WriteLine($"Output: \"{GetNotificationChannels(testCase)}\"");
+                Console.WriteLine();
+            }
+        }
+        static string GetNotificationChannels(string title)
+        {
+            
+            HashSet<string> validChannels = new HashSet<string> { "BE", "FE", "QA", "Urgent" };
+            
+            Regex regex = new Regex(@"\[([^\]]+)\]");
+            MatchCollection matches = regex.Matches(title);
+
+            HashSet<string> channels = new HashSet<string>();
+
+            foreach (Match match in matches)
+            {
+                string tag = match.Groups[1].Value;
+                if (validChannels.Contains(tag))
+                {
+                    channels.Add(tag);
+                }
+            }
+
+            return $"Receive channels: {string.Join(", ", channels)}";
+        }
+    }
+}
+
